a6b2391d3a6aa286be20bc199c29974ee8c633cb
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-06 17:06:13 +0100

    hasta level

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 window.onload = function() {
-  var canvasDistorsion = new CanvasDistorsion("canvasTv");
-  canvasDistorsion.start();
-  var canvasTime = new CanvasTime("canvasTimeBar");
-  canvasTime.start();
-  var gameselector = new GameSelector();
+  var gameSelector = new GameSelector();
+  gameSelector.start();
 };
-
-var answer
 
 function GameSelector() {
   this.correctMovie = {};
   this.possibleAnswerArray = [];
+}
+GameSelector.prototype.start = function() {
   this.randomObject();
   this.pushAnswerArray();
   this.takeMovie();
   this.buttonsDOM();
   this.imageDOM();
-}
+
+  var canvasDistorsion = new CanvasDistorsion("canvasTv");
+  canvasDistorsion.start();
+  var canvasTime = new CanvasTime("canvasTimeBar");
+  canvasTime.start();
+};
 
 GameSelector.prototype.randomObject = function() {
   this.correctMovie = movies[Math.floor(Math.random() * movies.length)];
+  // this.courseGame = new CourseGame(this.correctMovie);
 };
 
 GameSelector.prototype.pushAnswerArray = function() {
@@ -34,31 +37,39 @@ GameSelector.prototype.takeMovie = function() {
   ) {
     this.possibleAnswerArray.push(ranMovie);
   } else if (this.possibleAnswerArray.length == 4) {
-    // console.log(this.possibleAnswerArray)
     return;
   }
   this.takeMovie();
 };
 
 GameSelector.prototype.buttonsDOM = function() {
-  this.possibleAnswerArray.forEach(function(movie) {
-    $("#right-block").append(
-      $("<button/>", {
-        text: movie,
-        click: function(e) {
-          this.correctAnswer(e.target.innerHTML)
-        }.bind(this)
-      })
-    );
-  }.bind(this));
+  this.possibleAnswerArray.forEach(
+    function(movie) {
+      $("#right-block").append(
+        $("<button/>", {
+          text: movie,
+          click: function(e) {
+            this.correctAnswer(e.target.innerHTML);
+            // this.courseGame.correctAnswer(e.target.innerHTML);
+          }.bind(this)
+        })
+      );
+    }.bind(this)
+  );
 };
 
 GameSelector.prototype.imageDOM = function() {
   $("#imagen-movie").attr("src", this.correctMovie.image);
 };
 
-GameSelector.prototype.correctAnswer = function(movieTitle){
-    if (movieTitle === this.correctMovie.title){
-      alert("CORRECTO!!!!!!!")
-    }
+GameSelector.prototype.correctAnswer = function(movieTitle) {
+  if (movieTitle === this.correctMovie.title) {
+    alert("CORRECTO!!!!!!!");
+  } else {
+    this.lives -= 1;
+    alert("fallaste");
   }
+   
+  
+};
+
